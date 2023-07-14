a765e5e045fe74df04dcd0d5b167176d2b45eeb1
AlperenImam0glu <alperenimamoglu16@gmail.com>
2023-07-14 13:45:26 +0300

    ikonlar güncellendi

---
+++ app/src/main/java/com/example/marvelheroes/models/Comic.kt
@@ -0,0 +1,4 @@
+package com.example.marvelheroes.models
+
+class Comic {
+}
---
+++ app/src/main/java/com/example/marvelheroes/service/MarvelAPI.java
@@ -0,0 +1,12 @@
+package com.example.marvelheroes.service;
+
+import com.example.marvelheroes.models.Comic;
+
+import java.util.List;
+
+import io.reactivex.Single;
+import retrofit2.http.GET;
+
+interface MarvelAPI {
+
+}
