cab54d0d184b00a571400919a036b750272d9a8b
AlperenImam0glu <alperenimamoglu16@gmail.com>
2023-07-25 14:59:08 +0300

    HomePage Fragment Repository merged with Main Repository

--- app/src/main/java/com/example/marvelheroes/repository/MainRepository.kt
+++ app/src/main/java/com/example/marvelheroes/repository/MainRepository.kt
@@ -1,8 +1,16 @@
 package com.example.marvelheroes.repository
 
+import com.example.marvelheroes.databinding.FragmentHomePageBinding
 import com.example.marvelheroes.paging.network.RetrofitApi
 import com.example.marvelheroes.paging.network.RetrofitService
 
 class MainRepository {
     fun retroService(): RetrofitService =  RetrofitApi.apiService
+
+    fun scrollToCharacter(binding: FragmentHomePageBinding){
+        binding.scrollView.smoothScrollBy(0,binding.characterRecyclerView.top)
+    }
+    fun scrollToComics(binding: FragmentHomePageBinding){
+        binding.scrollView.smoothScrollBy(0,binding.comicsRecyclerView.bottom)
+    }
 }
