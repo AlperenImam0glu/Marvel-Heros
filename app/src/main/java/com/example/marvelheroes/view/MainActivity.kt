6ee3f0afa8d4f33d0796261f217754ffc5b56027
AlperenImam0glu <alperenimamoglu16@gmail.com>
2023-08-01 03:26:52 +0300

    Added Splash screen

--- app/src/main/java/com/example/marvelheroes/view/MainActivity.kt
+++ app/src/main/java/com/example/marvelheroes/view/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.marvelheroes.view
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.example.marvelheroes.R
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -9,6 +10,8 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Thread.sleep(1000)
+        installSplashScreen()
         setContentView(R.layout.activity_main)
     }
 
